158ee7f7526345de26bf907752a2b16c16317233
pushmufc <pushdev203@gmail.com>
2024-10-09 13:36:58 +0530

    Initial commit

---
+++ musiclibrary.py
@@ -0,0 +1,10 @@
+music = {
+    "pehli" : "https://www.youtube.com/watch?v=SxTYjptEzZs&pp=ygUFc29uZ3M%3D",
+    "shape" : "https://www.youtube.com/watch?v=JGwWNGJdvx8&pp=ygUFc29uZ3M%3D",
+    "khairiyat" : "https://www.youtube.com/watch?v=hoNb6HuNmU0&pp=ygUFc29uZ3M%3D"
+     
+}
+
+
+
+"https://newsapi.org/v2/top-headlines?country=us&apiKey=476629908fb942be9e44bd18de29c5a0"
